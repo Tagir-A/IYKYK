9110b79abc0eb18e42b3b5bc360937316b5e3a7a
Tagir <tccup1@gmail.com>
2023-11-26 23:24:11 +0100

    add basic home page plus test

--- src/pages/Home.test.tsx
+++ src/pages/Home.test.tsx
@@ -1,9 +1,55 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 
-test("renders Home page", () => {
-  render(<Home />);
-  const homeElement = screen.getByText(/Home Page/i);
-  expect(homeElement).toBeInTheDocument();
+test("renders home page with tasks and user list link", async () => {
+  render(
+    <MemoryRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </MemoryRouter>
+  );
+
+  // Assertions
+  const homeHeading = screen.getByText(/Home Page/i);
+  expect(homeHeading).toBeInTheDocument();
+
+  const tasksHeading = screen.getByText(/Tasks/i);
+  expect(tasksHeading).toBeInTheDocument();
+
+  const initialTask = await screen.findByText(/delectus aut autem/i);
+  expect(initialTask).toBeInTheDocument();
+
+  const userLink = screen.getByRole("link", { name: /Go to User List/i });
+  expect(userLink).toBeInTheDocument();
+});
+
+test("adds and deletes tasks", async () => {
+  render(
+    <MemoryRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </MemoryRouter>
+  );
+
+  // Add a task
+  const taskInput = screen.getByPlaceholderText(/Enter a new task/i);
+  fireEvent.change(taskInput, { target: { value: "New Task" } });
+  const addTaskButton = screen.getByText(/Add Task/i);
+  fireEvent.click(addTaskButton);
+
+  // Assertions after adding a task
+  const newTask = await screen.findByText(/New Task/i);
+  expect(newTask).toBeInTheDocument();
+
+  // Delete the task
+  const deleteButton = screen.getByText(/Delete/i);
+  fireEvent.click(deleteButton);
+
+  // Assertions after deleting the task
+  const deletedTask = screen.queryByText(/New Task/i);
+  expect(deletedTask).not.toBeInTheDocument();
 });
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
-  return <div>Home Page</div>;
+  const [tasks, setTasks] = useState<string[]>([]);
+  const [newTask, setNewTask] = useState<string>("");
+
+  useEffect(() => {
+    // Fetch initial tasks from JSONPlaceholder or any other API
+    const fetchTasks = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/todos"
+        );
+        const data = await response.json();
+        setTasks(data.slice(0, 5).map((task: any) => task.title));
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+      }
+    };
+
+    fetchTasks();
+  }, []);
+
+  const addTask = () => {
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+    }
+  };
+
+  const deleteTask = (index: number) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <div>
+        <h3>Tasks</h3>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              {task}
+              <button onClick={() => deleteTask(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Enter a new task"
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <Link to="/user-list">Go to User List</Link>
+    </div>
+  );
 };
 
 export default Home;
