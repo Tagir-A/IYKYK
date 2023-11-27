fceb0826103d878a037c424215f15f121aedccbd
Tagir <tccup1@gmail.com>
2023-11-27 11:36:39 +0100

    add codesplitting

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,17 +1,33 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import UserListPage from "./pages/UserListPage";
-import UserDetailsPage from "./pages/UserDetailsPage";
+
 import SlideRoutes from "react-slide-routes";
+
+const UserListPage = React.lazy(() => import("./pages/UserListPage"));
+const UserDetailsPage = React.lazy(() => import("./pages/UserDetailsPage"));
 
 const AppRouter: React.FC = () => {
   return (
     <Router>
       <SlideRoutes>
         <Route path="/" element={<Home />} />
-        <Route path="/user-list" element={<UserListPage />} />
-        <Route path="/user-details/:id" element={<UserDetailsPage />} />
+        <Route
+          path="/user-list"
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <UserListPage />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/user-details/:id"
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <UserDetailsPage />
+            </React.Suspense>
+          }
+        />
       </SlideRoutes>
     </Router>
   );
