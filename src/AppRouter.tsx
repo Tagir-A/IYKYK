baaff650006b8b09eded0961749e995187d4d62b
Tagir <tccup1@gmail.com>
2023-11-27 11:26:19 +0100

    add animation between routes

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,18 +1,18 @@
-// src/AppRouter.tsx
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import UserListPage from "./pages/UserListPage";
 import UserDetailsPage from "./pages/UserDetailsPage";
+import SlideRoutes from "react-slide-routes";
 
 const AppRouter: React.FC = () => {
   return (
     <Router>
-      <Routes>
+      <SlideRoutes>
         <Route path="/" element={<Home />} />
         <Route path="/user-list" element={<UserListPage />} />
         <Route path="/user-details/:id" element={<UserDetailsPage />} />
-      </Routes>
+      </SlideRoutes>
     </Router>
   );
 };
